ad1ac852c5144ef3a5ce0d38b43cdaba7a1e9ddf
Victor Costa <contato@victorfontes.com>
2014-05-25 09:55:48 -0300

    palpites: ok

--- www/js/controller.js
+++ www/js/controller.js
@@ -608,36 +608,6 @@ planetaBrasilControllers.controller('TeamPerGroupCtrl', ['$scope', '$http',
 );
 
 
-planetaBrasilControllers.controller('WorldChampionshipCtrl', ['$scope', '$http',
-    function ($scope, $rootScope, $http ) {
-        $scope.items = $rootScope.items;
-        language = localStorage.getItem('language');
-        $scope.guess = guess[language];
-
-        $scope.$on('$viewContentLoaded', function() {
-            body = document.body;
-            menuAchor = document.getElementsByClassName('menu')[0];
-            menuAchor.addEventListener("click", function(e) {
-                e.preventDefault();
-                if (body.classList.length == 0) {
-                    body.className = "menu-active";
-                }else {
-                    body.className = "";
-                };
-            });
-        });
-        document.getElementById('guess_id').onchange = function() {
-            alert('Fazer o post nesse change');
-        };
-        $scope.activeMenu = function(item) {
-            angular.forEach($rootScope.items, function(i) {
-                i.status = 'deactive';
-            });
-            item.status = 'active';
-            body.className = "";
-       };
-    }]
-);
 
 
 
@@ -679,5 +649,56 @@ planetaBrasilControllers.controller('PhotoFansCtrl', ['$scope', '$http',
       
       });
     
+    }]
+);
+
+
+planetaBrasilControllers.controller('WorldChampionshipCtrl', ['$scope', '$http',
+    function ($scope, $rootScope, $http ) {
+        $http = $rootScope
+        $scope.items = $rootScope.items;
+        language = localStorage.getItem('language');
+        $scope.guess = guess[language];
+
+        $scope.$on('$viewContentLoaded', function() {
+            body = document.body;
+            menuAchor = document.getElementsByClassName('menu')[0];
+            menuAchor.addEventListener("click", function(e) {
+                e.preventDefault();
+                if (body.classList.length == 0) {
+                    body.className = "menu-active";
+                }else {
+                    body.className = "";
+                };
+            });
+        });
+        
+        document.getElementById('guess_id').onchange = function() {
+            this.disabled = true;
+            var data = 'country=' + this.value;
+            var request = new XMLHttpRequest();
+            request.open('POST', API_ROOT_URL + '/api/guesses/', true);
+            request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            request.send(data);
+        };
+        
+        $scope.activeMenu = function(item) {
+            angular.forEach($rootScope.items, function(i) {
+                i.status = 'deactive';
+            });
+            item.status = 'active';
+            body.className = "";
+       };
+
+
+       var api_url = API_ROOT_URL + '/api/guesses/' + '?lang=' + language;
+        $http({method: 'GET', url: API_ROOT_URL + '/api/guesses/'}).
+            success(function(data, status, headers, config) {
+            $scope.guess = data[language];
+          }).
+          error(function(data, status, headers, config) {
+            alert('Ocorreu um erro. Tente novamente.')
+        });
+
     }]
 );
